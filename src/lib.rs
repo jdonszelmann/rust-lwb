d5f5023d9375a56c43cb9f9a64202b554fd3982b
jonay2000 <jonabent@gmail.com>
2022-01-29 23:10:57 +0100

    improve docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
+
+/// Contains code related to syntax definitions
+/// and parsing target languages based on these
+/// definitions. Also contains sort/constructor
+/// related code and code generation for these.
+/// Contains the PEG parser.
 pub mod parser;
+
+/// Helpers for type checking
 pub mod types;
+
+/// Helpers for code generation
 pub mod transform;
+
+/// Code spans (which reference to a source file)
 pub mod span;
+/// Source files (stored together with a name).
 pub mod source_file;
-
-#[test]
-fn test() {
-
-
-}
 
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,3 +1,8 @@
 
+/// Contains parse errors and methods for
+/// dealing with them.
 pub mod error;
+
+/// Contains code related to parsing syntax
+/// definition files
 pub mod syntax_file;
