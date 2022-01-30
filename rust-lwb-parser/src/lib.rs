06c8641dfc02b1fded837797437f630332bcbe64
jonay2000 <jonabent@gmail.com>
2022-01-31 00:33:24 +0100

    add stuff

---
+++ rust-lwb-parser/src/lib.rs
@@ -0,0 +1,16 @@
+/// Contains code related to syntax definitions
+/// and parsing target languages based on these
+/// definitions. Also contains sort/constructor
+/// related code and code generation for these.
+/// Contains the PEG parser.
+pub mod parser;
+
+/// Source files (stored together with a name).
+pub mod source_file;
+/// Code spans (which reference to a source file)
+pub mod span;
+
+/// Code related to generating rust source files
+/// from language definitions. Usually used from
+/// build.rs files.
+pub mod codegen;
